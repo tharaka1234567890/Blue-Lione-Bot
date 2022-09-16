84cd3e8116c9af1b818037e0d308d9e3e954dcc3
tharaka1234567890 <111579902+tharaka1234567890@users.noreply.github.com>
2022-09-16 17:21:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -13,7 +13,7 @@ global.APIKeys = {
 
 
 global.OWNER_REACT = '😈💻'
-global.REACT_NUMBER = '94743486523'
+global.REACT_NUMBER = '94714317675'
 
 //aumto functioner
 global.autoTyping = false //auto tying in gc (true to on, false to off)
@@ -34,15 +34,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['94743486523'] //ur owner number
+global.vcardowner = ['94714317675'] //ur owner number
 global.ownername = "𝙈𝘼𝙃𝘼𝙕𝙊𝙉𝙀😈💫 " //ur owner name
 global.ytname = "YT: Nipuna-apps" //ur yt chanel name
 global.socialm = "GitHub: Nipuna-apps" //ur github or insta name
 global.location = "srilanka,kurunagala,nikaweratiya" //ur location
 
 //bot bomdy 
-global.owner = ['94743486523']
-global.ownertag = '94743486523' //ur tag number
+global.owner = ['94714317675']
+global.ownertag = '94714317675' //ur tag number
 global.botname = '𝙈𝘼𝙃𝘼𝙕𝙊𝙉𝙀😈💫' //ur bot name
 global.linkz = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //your theme url which will be displayed on whatsapp
 global.websitex = "https://github.com/Nipuna-apps/Blue-Lione-Bot.git" //ur website to be displayed
@@ -59,7 +59,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['94743486523'] //ur premium numbers
+global.premium = ['94714317675'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
